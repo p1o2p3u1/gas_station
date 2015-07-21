19f61a6cda712ca6e5904c0dc30c40f4c82f5311
ting.wu <ting.wu@corp.netease.com>
2015-07-21 15:21:18 +0800

    at least it works now.

--- file_handler.py
+++ file_handler.py
@@ -1,4 +1,6 @@
 import os
+from os import walk, path
+from os.path import normpath
 
 class FileHandler:
 
@@ -8,13 +10,53 @@ class FileHandler:
     def list_all_files(self):
         """
         list all the files and directories for a given path.
-        :return: {
-            'path': 'E:/projects/gitlab/biubiu'
-
+        ignore .svn, .git, .gitignore, *.pyc
+        :return: e.g.: {
+            'path': '/home/source'
+            'dirs': ['dir1', 'dir2', 'dir3'],
+            'files': ['file1', 'file2', 'file3'],
+            'dir1': ['/home/source/dir1/f1.py', '/home/source/dir1/a/f2.py'],
+            'dir2': ['/home/source/dir3/f1.py', '/home/source/dir3/f2.py', '/home/source/dir3/aa/bb/cc/dd.py']
         }
         """
-        result = {'path': self.path}
-        for dirname, dirnames, filenames in os.walk(self.path):
-            pass
+        result = {
+            'path': self.path,
+            'dirs': [],
+            'files': []
+        }
+        for dirname, dirs, files in walk(self.path):
+            for d in dirs:
+                if not d.startswith('.'):
+                    result['dirs'].append(d)
+            for f in files:
+                if not d.startswith('.') and d.endswith('.py'):
+                    result['files'].append(f)
+            break
 
+        for t_dir in result['dirs']:
+            result[t_dir] = []
+            sub_path = path.join(self.path, t_dir)
+            for dirname, dirs, files in walk(sub_path):
+                for t_file in files:
+                    if not t_file.startswith('.') and t_file.endswith('.py'):
+                        location = path.join(t_dir, t_file)
+                        result[t_dir].append(normpath(location))
+        return result
+
+    def get_source(self, filename):
+        """
+        get the source text of the file
+        :param filename: the name of the source file.
+        :return: e.g. {
+            'filename': 'a.py',
+            'text': "print 'hi'"
+            'revision': 1234
+        }
+        """
+        result = {
+            'filename': filename
+        }
+        location = path.join(self.path, filename)
+        with open(location, 'r') as reader:
+            result['text'] = reader.read()
         return result
--- server.py
+++ server.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify, request, current_app
+from functools import wraps
+from file_handler import FileHandler
 
 app = Flask(__name__)
+
+def jsonp(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            data = str(func(*args, **kwargs).data)
+            content = str(callback) + '(' + data + ')'
+            mimetype = 'application/javascript'
+            return current_app.response_class(content, mimetype=mimetype)
+        else:
+            return func(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
 def hello():
     return "hello world"
 
+@app.route('/file/<filename>')
+@jsonp
+def get_source(filename):
+    f = FileHandler()
+    return jsonify(f.get_source(filename))
+
+@app.route('/list')
+@jsonp
+def list_dir():
+    f = FileHandler()
+    return jsonify(f.list_all_files())
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
